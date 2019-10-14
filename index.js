7b1cbe75b5784609bcbf5781a42d5373cb32fa53
Sri <795279+srikanthmanda@users.noreply.github.com>
2019-10-14 22:49:15 +0530

    Created proper web UI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { formatObject } from './format_json.js'
 export function parseJSON() {
     const unformattedJSON = document.getElementById('json_text').value;
     try {
-        alert(formatObject(JSON.parse(unformattedJSON)));
+        document.getElementById('formatted_json_text').value = formatObject(JSON.parse(unformattedJSON));
     } catch (e) {
         if (e instanceof SyntaxError) {
             alert('Invalid JSON');
